d64beaa59de74771e7ddb3e94c23341736773236
lyfeyaj <lyfeyaj@gmail.com>
2013-08-29 10:05:30 +0800

    add ls alias

--- bash/alias.sh
+++ bash/alias.sh
@@ -2,4 +2,8 @@ BUNDLED_COMMANDS="rackup rails rake rspec guard ruby spork thin unicorn annotate
 [ -f $HOME/.bash/bundler-exec.sh ] && source $HOME/.bash/bundler-exec.sh
 
 alias ls-al='ls -al'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
 alias javac='LC_ALL=en javac'
